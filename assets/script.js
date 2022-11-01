0982fe38a5d9857fc80a70b231cc191b8f6187de
marysgreenwood <marysgreenwood@gmail.com>
2022-10-31 20:23:30 -0400

    added carousel code

--- assets/script.js
+++ assets/script.js
@@ -112,10 +112,26 @@ var getParkById = function(event){
       parkActivities.innerHTML = parkActivities.innerHTML + ', ' + data.data[0].activities[i].name;
     }
     //parkLink.attr('href', data.data[0].url)
-  })
+    parkImages=[];
+    for (var i=0; i<data.data[0].images.length; i++){
+      parkImages.push({url: data.data[0].images[i].url, altText: data.data[0].images[i].altText});
+    }
+    console.log(parkImages)
+    //store carousel items in array
+    var slideOne=document.getElementById('first-slide');
+    var slideTwo=document.getElementById('second-slide');
+    var slideThree=document.getElementById('third-slide');
+    var slideFour=document.getElementById('fourth-slide');
+    var slides=[slideOne, slideTwo, slideThree, slideFour];
+    for (var i=0; i<slides.length; i++){
+        slides[i].setAttribute("src", parkImages[i].url);
+        slides[i].setAttribute("alt", parkImages[i].altText);
+      }
+    })
   .catch(function (error){
     alert('It didnt work again');
   })
+  
 }
 
 //Gets five day forecast and displays on the weather display
